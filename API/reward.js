49edd9f12921447195a89240b41781898bcf87fb
cardiojay497-cmd <cardiojay497@gmail.com>
2026-09-17 15:51:32 +0100

    Delete API directory

--- API/reward.js
+++
@@ -1,5 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  const userId = req.query.userId || 'test';
-  return res.status(200).json({ ok: true, userId: userId });
-}
